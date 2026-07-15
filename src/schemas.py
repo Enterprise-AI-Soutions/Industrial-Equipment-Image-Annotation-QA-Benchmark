c569eb02899259451613e1ba9b568581053fb7b7
goodgentleman918 <goodgentleman918@gmail.com>
2026-07-15 15:40:22 +0530

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -1,1 +1,12 @@
+from pydantic import BaseModel, Field
 
+
+class Annotation(BaseModel):
+
+    image_id: str
+
+    label: str
+
+    bbox: list[int] = Field(min_length=4, max_length=4)
+
+    annotator: str
