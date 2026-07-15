d43b315acbd4159cf5b3fe7a1295823c881c154c
goodgentleman918 <goodgentleman918@gmail.com>
2026-07-15 15:28:23 +0530

    Update test_validation.py

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,1 +1,13 @@
+from src.schemas import Annotation
 
+
+def test_annotation_creation():
+    annotation = Annotation(
+        image_id="IMG001",
+        label="air_compressor",
+        bbox=[20, 30, 200, 150],
+        annotator="ANN001"
+    )
+
+    assert annotation.image_id == "IMG001"
+    assert annotation.label == "air_compressor"
